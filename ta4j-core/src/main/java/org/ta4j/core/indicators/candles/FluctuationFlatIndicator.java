89e7ce4e7332e03949ef71d7265a24908edff97c
mjime127 <mjime127@corp.vodafone.es>
2022-06-22 20:45:10 +0200

    Modo fluctuación

--- ta4j-core/src/main/java/org/ta4j/core/indicators/candles/FluctuationFlatIndicator.java
+++ ta4j-core/src/main/java/org/ta4j/core/indicators/candles/FluctuationFlatIndicator.java
@@ -10,17 +10,30 @@ import org.ta4j.core.num.Num;
  */
 public class FluctuationFlatIndicator extends CachedIndicator<Boolean> {
 
+	public final static String MODE_EQUAL_OHLC = "OHLC";
+	public final static String MODE_EQUAL_OC = "OC";
+	
+	public String mode;
+	
     /**
      * Constructor.
      *
      * @param series a bar series
      */
-    public FluctuationFlatIndicator(BarSeries series) {
+    public FluctuationFlatIndicator(BarSeries series, String mode) {
         super(series);
+        this.mode = mode;
     }
 
     @Override
     protected Boolean calculate(int index) {
+    	
+    	Boolean resultado = null;
+    	
+    	if (this.mode == null) {
+    		this.mode = FluctuationFlatIndicator.MODE_EQUAL_OHLC;
+    	}
+    	
         if (index < 1) {
             return false;
         }
@@ -31,8 +44,19 @@ public class FluctuationFlatIndicator extends CachedIndicator<Boolean> {
         final Num currOpenPrice = currBar.getOpenPrice();
         final Num currClosePrice = currBar.getClosePrice();
         
-        return currOpenPrice.isEqual(currHighPrice) && currOpenPrice.isEqual(currLowPrice)
-               && currClosePrice.isEqual(currOpenPrice) && currClosePrice.isEqual(currClosePrice);
+        if (FluctuationFlatIndicator.MODE_EQUAL_OHLC.equals(this.mode)) { // Debe ser igual OHLC
+        	resultado = currOpenPrice.isEqual(currHighPrice) && currOpenPrice.isEqual(currLowPrice)
+        			    && currClosePrice.isEqual(currOpenPrice) && currClosePrice.isEqual(currClosePrice);
+        	
+        } else if (FluctuationFlatIndicator.MODE_EQUAL_OC.equals(this.mode)) { // Sólo debe ser igual OC
+        	resultado = currClosePrice.isEqual(currOpenPrice) && currClosePrice.isEqual(currClosePrice);
+        	
+        } else { // Cualquier otro valor se tomará modo estricto y debe ser igual OHLC
+        	resultado = currOpenPrice.isEqual(currHighPrice) && currOpenPrice.isEqual(currLowPrice)
+    			    && currClosePrice.isEqual(currOpenPrice) && currClosePrice.isEqual(currClosePrice);        	
+        }
+        
+        return resultado;
         
         
     }
